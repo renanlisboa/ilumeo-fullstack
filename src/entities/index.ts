cd77f86f1c56fdb6784fb3803dab28b5ef065c72
renanlisboa <renlp89.developer@gmail.com>
2023-03-18 01:30:32 -0300

    feat: move ponto entity to its own file

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,1 +1,2 @@
 export * from './colaborador.entity';
+export * from './ponto.entity';
---
+++ src/entities/ponto.entity.ts
@@ -0,0 +1,45 @@
+type PontoBody = {
+  colaboradorId: string;
+  entrada?: Date;
+  saida?: Date;
+};
+
+export class Ponto {
+  private readonly colaboradorId: string;
+  private readonly entrada?: Date;
+  private saida?: Date;
+
+  constructor(body: PontoBody) {
+    this.validarBody(body);
+    this.colaboradorId = body.colaboradorId;
+    this.entrada = body.entrada;
+    this.saida = body.saida;
+  }
+
+  getData(): PontoBody {
+    return {
+      colaboradorId: this.colaboradorId,
+      entrada: this.entrada,
+      saida: this.saida,
+    };
+  }
+
+  setSaida(saida: Date): void {
+    this.validarHoraEntradaESaida(saida);
+    this.saida = saida;
+  }
+
+  validarBody(body: PontoBody): void {
+    if (!body.colaboradorId) throw new Error('colaborador_id é obrigatório');
+    if (!body.entrada && !body.saida) {
+      throw new Error('entrada ou saída é obrigatório');
+    }
+  }
+
+  validarHoraEntradaESaida(saida: Date): void {
+    if (!this.entrada) return;
+    if (saida <= this.entrada) {
+      throw new Error('data de entrada maior ou igual a data de saída');
+    }
+  }
+}
