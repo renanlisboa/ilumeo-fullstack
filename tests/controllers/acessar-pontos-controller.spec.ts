5ff6d1d9c29301e93fe32b05ce5123eaf507483b
renanlisboa <renlp89.developer@gmail.com>
2023-03-19 11:38:57 -0300

    test: should return status code 201 when required data is passed throgh the request body

--- tests/controllers/acessar-pontos-controller.spec.ts
+++ tests/controllers/acessar-pontos-controller.spec.ts
@@ -1,25 +1,46 @@
-import { it, expect } from 'vitest';
+import { describe, it, expect } from 'vitest';
 
 import { ColaboradorMemoryRepository } from '../../src/infra/repositories/memory';
 import { AcessarPontos } from '../../src/application/usecases';
 import { AcessarPontosZodValidator } from '../../src/validators/zod';
 import { AcessarPontosController } from '../../src/presentation/controllers';
 
-it('Deve retornar status 400 se o código do colaborador for inválido', async () => {
-  const colaboradorMemoryRepository = new ColaboradorMemoryRepository();
-  const acessarPontos = new AcessarPontos(colaboradorMemoryRepository);
-  const validator = new AcessarPontosZodValidator();
-  const acessarPontosController = new AcessarPontosController(
-    validator,
-    acessarPontos,
-  );
-  const requisicao = {
-    body: {
-      codigoColaborador: '',
-    },
-  };
+describe('AcessarPontosController', () => {
+  it('Deve retornar status 400 se o código do colaborador for inválido', async () => {
+    const colaboradorMemoryRepository = new ColaboradorMemoryRepository();
+    const acessarPontos = new AcessarPontos(colaboradorMemoryRepository);
+    const validator = new AcessarPontosZodValidator();
+    const acessarPontosController = new AcessarPontosController(
+      validator,
+      acessarPontos,
+    );
+    const requisicao = {
+      body: {
+        codigoColaborador: '',
+      },
+    };
 
-  const output = await acessarPontosController.handle(requisicao);
+    const output = await acessarPontosController.handle(requisicao);
 
-  expect(output.statusCode).toBe(400);
+    expect(output.statusCode).toBe(400);
+  });
+
+  it('Deve retornar status 200 se o código do colaborador for válido', async () => {
+    const colaboradorMemoryRepository = new ColaboradorMemoryRepository();
+    const acessarPontos = new AcessarPontos(colaboradorMemoryRepository);
+    const validator = new AcessarPontosZodValidator();
+    const acessarPontosController = new AcessarPontosController(
+      validator,
+      acessarPontos,
+    );
+    const requisicao = {
+      body: {
+        codigoColaborador: '4SXXFMf',
+      },
+    };
+
+    const output = await acessarPontosController.handle(requisicao);
+
+    expect(output.statusCode).toBe(200);
+  });
 });
