48b114892e790e960c32ff12dc75fd86759da618
renanlisboa <renlp89.developer@gmail.com>
2023-03-25 00:50:24 -0300

    feat: add stream

--- src/main/routes/ponto.routes.ts
+++ src/main/routes/ponto.routes.ts
@@ -8,6 +8,26 @@ import {
 
 const pontoRoutes = RouterAdapter();
 
+pontoRoutes.get('/stream', (request, response) => {
+  const buscarPonto = makeBuscarPontoFactory(request);
+  buscarPonto
+    .then(({ data }) => {
+      response.setHeader('Content-Type', 'text/event-stream');
+      response.setHeader('Cache-Control', 'no-cache');
+      response.setHeader('Connection', 'keep-alive');
+      const interval = setInterval(async () => {
+        response.write('event: message\n');
+        response.write(`data: ${JSON.stringify(data)}`);
+        response.write('\n\n');
+      }, 5000);
+      response.on('close', () => {
+        clearInterval(interval);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 pontoRoutes.get('/listar-pontos', adaptRoute(makeListarPontosFactory));
 pontoRoutes.get('/buscar-ponto', adaptRoute(makeBuscarPontoFactory));
 pontoRoutes.post('/registrar-entrada', adaptRoute(makeRegistrarEntradaFactory));
